ffa22fc83f935bbff0754db7b3cacc71cdb311fc
D3PSI <35960796+D3PSI@users.noreply.github.com>
2019-08-03 18:18:56 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ echo "Installing nasa-wallpaper-changer..."
 cp -R nasa-wallpaper-changer/ /usr/local/bin/nasa-wallpaper-changer
 echo "Installing new crontab..."
 crontab -l > mycron
-echo "@reboot python3 -m /usr/local/bin/nasa-wallpaper-changer/nasa-wallpaper-changer" >> mycron
-echo "@hourly python3 -m /usr/local/bin/nasa-wallpaper-changer/nasa-wallpaper-changer" >> mycron
-echo "0 0 * * * python3 -m /usr/local/bin/nasa-wallpaper-changer/nasa-wallpaper-changer" >> mycron
+echo "@reboot python3 /usr/local/bin/nasa-wallpaper-changer/nasa-wallpaper-changer.py" >> mycron
+echo "@hourly python3 /usr/local/bin/nasa-wallpaper-changer/nasa-wallpaper-changer.py" >> mycron
+echo "0 0 * * * python3 /usr/local/bin/nasa-wallpaper-changer/nasa-wallpaper-changer.py" >> mycron
 crontab mycron
 rm mycron
 echo "Successfully installed new crontab"
