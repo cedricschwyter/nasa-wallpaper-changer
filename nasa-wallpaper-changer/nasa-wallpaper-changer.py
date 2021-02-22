c5e47618032f4bba9e0da80ec0b25f04289a8ebc
Cedric Schwyter <cedricschwyter@bluewin.ch>
2021-02-22 14:22:46 +0100

    (#1) Fix missing time import

--- nasa-wallpaper-changer/nasa-wallpaper-changer.py
+++ nasa-wallpaper-changer/nasa-wallpaper-changer.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
-import os, getpass
+import getpass
+import os
 import subprocess
-import schedule
+import time
 import urllib.request as url
-from bs4 import BeautifulSoup
 
+import schedule
+from bs4 import BeautifulSoup
 
 USER = getpass.getuser()
 ROOT_URL = "https://apod.nasa.gov/apod/"
@@ -14,71 +16,61 @@ IMG_STUB = ""
 
 
 def get_nasa_iotd_site():
-
     print("Connecting to NASA's Astronomy Picture Of The Day page...")
     file = url.urlopen(ROOT_URL + "astropix.html")
     print("Successfully connected to NASA's Astronomy Picture Of The Day page")
-    site = file.read().decode('UTF-8')
+    site = file.read().decode("UTF-8")
     print("Successfully decoded NASA's Astronomy Picture Of The Day page")
     file.close()
     print("Closing connection...")
-
     return site
 
 
 def extract_iotd_from_nasa_iotd_site():
-
     site = get_nasa_iotd_site()
     print("Parsing site...")
     parser = BeautifulSoup(site, "html.parser")
-    img = parser.find('img')
-
-    return img['src']
+    img = parser.find("img")
+    return img["src"]
 
 
 def set_gnome_wallpaper(file_path_):
-
-    subprocess.Popen("DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format(file_path_), shell=True)
+    subprocess.Popen(
+        "DISPLAY=:0 GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri file://{0}".format(
+            file_path_
+        ),
+        shell=True,
+    )
 
 
 def main():
-
     print("Starting nasa-wallpaper-changer service...")
     image = extract_iotd_from_nasa_iotd_site()
     image_url = ROOT_URL + image
     print("Image URL: " + image_url)
     image_data = url.urlopen(image_url).read()
-
     try:
-
         os.mkdir(IMG_DIR)
-
     except FileExistsError:
-
         pass
-
-    onlyfiles = next(os.walk(IMG_DIR))[2] 
+    onlyfiles = next(os.walk(IMG_DIR))[2]
     IMG_STUB = "nasa-iotd-" + str(len(onlyfiles)) + ".jpg"
-    image_disk = open(IMG_DIR + IMG_STUB, "bw+")      # Open file in binary and overwrite mode
+    image_disk = open(
+        IMG_DIR + IMG_STUB, "bw+"
+    )  # Open file in binary and overwrite mode
     image_disk.write(image_data)
     image_disk.close()
     print("Successfully written to destination directory")
     print("Attempting to set as background...")
     try:
-
         set_gnome_wallpaper(IMG_DIR + IMG_STUB)
         print("Successfully set as background")
-
     except:
-
         print("Failed to set as background")
 
 
 if __name__ == "__main__":
-
     schedule.every().hour.do(main)
-
     while True:
-
         schedule.run_pending()
         time.sleep(120)
